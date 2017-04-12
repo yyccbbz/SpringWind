05ffc4d4730d5e534cbbf5d725c8950151eb9d1b
yyccbbz <juancan@163.com>
2017-04-12 18:17:04 +0800

    excel上传开发

--- src/main/java/com/baomidou/springwind/controller/UserController.java
+++ src/main/java/com/baomidou/springwind/controller/UserController.java
@@ -4,21 +4,26 @@ import com.baomidou.kisso.annotation.Action;
 import com.baomidou.kisso.annotation.Permission;
 import com.baomidou.kisso.common.encrypt.SaltEncoder;
 import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.springwind.common.result.AjaxResult;
 import com.baomidou.springwind.common.utils.StringUtil;
 import com.baomidou.springwind.common.view.SpringMvcExcelView;
 import com.baomidou.springwind.entity.User;
 import com.baomidou.springwind.service.IRoleService;
 import com.baomidou.springwind.service.IUserService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -39,8 +44,23 @@ public class UserController extends BaseController {
     @Autowired
     private IRoleService roleService;
 
+    //excel导出的字段
     @Value("${user.fields}")
     private String userFields;
+
+    //上传文件的类型
+    private static final HashMap<String, String> TypeMap = new HashMap<String, String>();
+
+    static {
+        /*TypeMap.put("image", "gif,jpg,jpeg,png,bmp");
+        TypeMap.put("flash", "swf,flv");
+        TypeMap.put("media", "swf,flv,mp3,wav,wma,wmv,mid,avi,mpg,asf,rm,rmvb");
+        TypeMap.put("file", "doc,docx,xls,xlsx,ppt,pptx,htm,html,txt,dwg,pdf");*/
+        TypeMap.put("file", "xls,xlsx");
+    }
+
+    //上传文件的最大值
+    public static long fileSize = 30 * 1024 * 1024;
 
     @Permission("2001")
     @RequestMapping("/list")
@@ -121,6 +141,91 @@ public class UserController extends BaseController {
         view.addObject(SpringMvcExcelView.EXCEL_EMPTY_MESSAGE,"XXX没有相关数据可以导出");
         return view;
     }
+
+    /**
+     * Excel导入
+     *
+     * @param file
+     * @param request
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "importExcel", method = RequestMethod.POST)
+    public AjaxResult importExcel(@RequestParam("fileExcel") CommonsMultipartFile file, HttpServletRequest request) {
+
+//        LOGGER.debug("该上传excel文件的原文件名是 :" + file.getOriginalFilename());
+
+        AjaxResult result = AjaxResult.success("导入数据成功...");
+
+        /*if (!file.isEmpty()) {
+
+            //判断请求类型是否为文件上传类型
+            if (!ServletFileUpload.isMultipartContent(request)) {
+                result.setCode(AjaxResult.CODE_FAILURE);
+                result.setMsg("该请求上传文件失败...");
+                return result;
+            }
+
+            //当文件超过设置的大小时，则不运行上传
+            if (file.getSize() > fileSize) {
+                result.setCode(AjaxResult.CODE_FAILURE);
+                result.setMsg("该上传文件大小超限制...");
+                return result;
+            }
+
+            //获取文件名后缀
+            String OriginalFilename = file.getOriginalFilename();
+            String fileSuffix = OriginalFilename.substring(OriginalFilename.lastIndexOf(".") + 1).toLowerCase();
+//            LOGGER.debug("该上传文件的后缀名为 :" + fileSuffix);
+
+            //判断该类型的文件是否在允许上传的文件类型内
+            if (!Arrays.asList(TypeMap.get("file").split(",")).contains(fileSuffix)) {
+                result.setCode(AjaxResult.CODE_FAILURE);
+                result.setMsg("请检查上传文件的格式...");
+                return result;
+            }
+
+            try {
+                // 获取Excel对象
+                ClExcel excel = ExcelHandlerUtil.getExcelFile(file, request);
+                //当前上传用户的id
+                excel.setUserId(88888888L);
+                excel.setCtime(new Date());
+                result.setObj(excel);
+
+                // 读取excel文件
+                String excelType = "clExtUserBak";
+                ExcelImportResult readExcel = excelContext.readExcel(excelType, file.getInputStream());
+                List<ClExtUserBak> listBean = readExcel.getListBean();
+                System.out.println("listBean = " + listBean);
+                this.clExtUserBakService.importExcelData(listBean, excel);
+
+                // 存储excel文件
+                UploadUtil.copy(file, excel.getExcelRealPath(), excel.getExcelRealName());
+            } catch (Exception e) {
+                result.setCode(AjaxResult.CODE_FAILURE);
+                if (e instanceof ExcelException) {
+                    result.setMsg(e.getMessage());
+                } else {
+                    if (e instanceof InvalidFormatException) {
+                        result.setMsg("错误的文件格式...");
+                    } else {
+                        result.setMsg(e.getMessage());
+                        e.printStackTrace();
+                        LOGGER.error(e.getMessage());
+                    }
+                }
+            }
+        } else {
+            //上传文件为空，或者当前登录用户不是投资顾问，则无权限上传
+            result.setCode(AjaxResult.CODE_DENIED);
+            result.setMsg("无法上传，请检查该上传文件或您的登录账户权限！");
+        }*/
+
+        System.out.println("返回页面的结果对象为result = " + result);
+        return result;
+    }
+
 
     @ResponseBody
     @Permission("2001")
--- src/main/webapp/WEB-INF/static/js/common.js
+++ src/main/webapp/WEB-INF/static/js/common.js
@@ -18,7 +18,7 @@ function formatDate(time) {
 
 
 /**
- * 下载文件,以POST的方式提交
+ * 下载文件,以POST的方式提交---同一个页面
  * @param options{url,data}
  * 使用方式
  * downLoadFile({
@@ -42,7 +42,7 @@ var downLoadFile = function (options) {
     $form[0].submit();
 };
 
-/**下载文件
+/**下载文件---新页面
  * options:{
  * url:‘‘,              //下载地址
  * data:{name:value},   //要发送的数据
@@ -63,7 +63,35 @@ var DownLoadFile = function (options) {
     $iframe.remove();
 }
 
-
+/**
+ * ajax上传文件
+ */
+var ajaxFileUpload = function(url,fileId) {
+    $.ajaxFileUpload({
+        url: url,                              //用于文件上传的服务器端请求地址
+        type: "post",                          //post请求方式
+        secureuri: false,                      //一般设置为false
+        fileElementId: "fileExcel",            //文件上传空间的id属性
+        dataType: "json",                      //返回值类型 一般设置为json
+        success: function (data, status) {     //服务器成功响应处理函数
+            console.log(data);
+            //刷新当前页
+            reload();
+            /** 0:失败,1:成功,2:未登录或session过期,3:无权限 */
+            if (data.code == 0) {
+                alert(data.msg);
+            } else if (data.code == 1) {
+                var excelName = data.obj.excelName;
+                alert(excelName + " ,文件成功上传 ，" + data.msg);
+            } else {
+                alert(data.msg);
+            }
+        },
+        error: function (data, status, e) {     //服务器响应失败处理函数
+            alert(data.msg + "---" + e);
+        }
+    });
+};
 
 
 
