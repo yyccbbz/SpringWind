35edd3aa5c4b4feae2d26c554f7287c1c45a0f66
juancan@163.com <zizou2015>
2017-05-23 16:59:10 +0800

    缓存优化

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -8,8 +8,8 @@ public class Test{
 
     public static void main(String[] args) {
 
-        Date randomDate = DateUtil.randomDate("2007-01-01", "2007-05-01");
-        System.out.println(randomDate.toString());
+        String s = System.getProperty("java.io.tmpdir");
+        System.out.println("s = " + s);
 
     }
 
