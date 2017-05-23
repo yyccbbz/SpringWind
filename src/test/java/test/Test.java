d7f597fbbfdb91336ebf9362f7a9225b94c5ef9f
yyccbbz <juancan@163.com>
2017-05-24 01:27:49 +0800

    解决启动问题

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,16 +1,35 @@
 package test;
 
-import com.baomidou.springwind.common.utils.DateUtil;
-
-import java.util.Date;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 
 public class Test{
 
+    public static CacheManager cacheManager = CacheManager.create();
+
     public static void main(String[] args) {
 
-        String s = System.getProperty("java.io.tmpdir");
-        System.out.println("s = " + s);
+        // Create a cache manager
+//        CacheManager cacheManager = new CacheManager();
 
+        // create the cache called "helloworld"
+        Cache cache = cacheManager.getCache("helloworld");
+
+        // create a key to map the data to
+        String key = "greeting";
+
+        // Create a data element
+        Element putGreeting = new Element(key, "Hello, World!");
+
+        // Put the element into the data store
+        cache.put(putGreeting);
+
+        // Retrieve the data element
+        Element getGreeting = cache.get(key);
+
+        // Print the value
+        System.out.println(getGreeting.getObjectValue());
     }
 
 }
