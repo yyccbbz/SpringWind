eedfa49b77016f2f65636c425a2714065d720d59
yyccbbz <juancan@163.com>
2017-06-26 00:38:06 +0800

    修改

--- src/main/java/com/baomidou/springwind/controller/ProductExpiresController.java
+++ src/main/java/com/baomidou/springwind/controller/ProductExpiresController.java
@@ -13,6 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/statsReport/productExpires")
-public class ProductExpiresController {
+public class ProductExpiresController extends BaseController {
 	
 }
