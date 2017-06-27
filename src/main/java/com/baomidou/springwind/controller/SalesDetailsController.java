92a45123bc9a7790ee739c35eb74b030c3620278
nathanyang0226 <7762366Yj>
2017-06-28 00:06:43 +0800

    新增销售明细编辑以及导出功能

--- src/main/java/com/baomidou/springwind/controller/SalesDetailsController.java
+++ src/main/java/com/baomidou/springwind/controller/SalesDetailsController.java
@@ -5,21 +5,23 @@ import com.baomidou.kisso.annotation.Permission;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.springwind.common.utils.DateUtil;
 import com.baomidou.springwind.common.utils.StringUtil;
+import com.baomidou.springwind.common.view.SpringMvcExcelView;
 import com.baomidou.springwind.entity.SalesDetails;
 import com.baomidou.springwind.entity.vo.SalesDetailVo;
 import com.baomidou.springwind.service.ISalesDetailsService;
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * <p>
@@ -31,13 +33,18 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/pfmReport/salesDetails")
-public class
-
-SalesDetailsController extends BaseController {
+public class SalesDetailsController extends BaseController {
 
     @Autowired
     private ISalesDetailsService salesDetailsService;
 
+    //excel-config.xml中配置的ID
+    @Value("${salesDetails.excelId}")
+    private String salesDetailsExcelId;
+
+    //excel导出的字段
+    @Value("${salesDetails.fields}")
+    private String salesDetailsFields;
 
     /**
      * 页面跳转
@@ -68,7 +75,7 @@ SalesDetailsController extends BaseController {
      */
     @ResponseBody
     @Permission("6001")
-    @RequestMapping(value = "/getUserList")
+    @RequestMapping(value = "/getSalesDetailList")
     public String getUserList(@RequestParam("_search") String _search) {
 
         System.err.println("筛选条件 formData =" + _search);
@@ -83,6 +90,58 @@ SalesDetailsController extends BaseController {
         Page<SalesDetails> page = getPage();
         Page<SalesDetails> userPage = salesDetailsService.selectPageByParams(page, salesDetailVo);
         return jsonPage(userPage);
+    }
+
+    @ResponseBody
+    @Permission("6001")
+    @RequestMapping(value = "/editSalesDetail",method = RequestMethod.POST)
+    public String editSalesDetail(SalesDetails salesDetails) {
+        boolean rlt = false;
+        if (salesDetails != null) {
+            if (salesDetails.getId() != null) {
+                rlt = salesDetailsService.updateById(salesDetails);
+            } else {
+                salesDetails.setCreateTime(new Date());
+                salesDetails.setUpdateTime(salesDetails.getCreateTime());
+                rlt = salesDetailsService.insert(salesDetails);
+            }
+        }
+        return callbackSuccess(rlt);
+    }
+
+
+    /**
+     * Excel导出列表
+     *
+     * @return
+     */
+    @Permission("6001")
+    @RequestMapping(value = "/downloadExcel",method = RequestMethod.POST)
+    public ModelAndView downloadExcel(){
+
+        /**1.执行你的业务逻辑获取数据，使用ExcelContent生成Workbook，需要四个参数:
+         *
+         * ①id 配置ID
+         * ②beans 配置class对应的List
+         * ③header 导出之前,在标题前面做出一些额外的操作,比如增加文档描述等,可以为null
+         * ④fields 指定Excel导出的字段(bean对应的字段名称),可以为null
+         */
+        Workbook workbook = null;
+        String id = salesDetailsExcelId;
+        List<SalesDetails> list = salesDetailsService.selectList(null);
+        List<String> fields = Arrays.asList(salesDetailsFields.split(","));
+        try {
+            workbook = excelContext.createExcel(id, list, null, fields);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /**2.跳转到Excel下载视图*/
+        ModelAndView view = new ModelAndView("springMvcExcelView");
+        view.addObject(SpringMvcExcelView.EXCEL_NAME, "销售明细" + DateUtil.getCurrentTime());
+        view.addObject(SpringMvcExcelView.EXCEL_WORKBOOK, workbook);
+        view.addObject(SpringMvcExcelView.EXCEL_EMPTY_MESSAGE, "销售明细 没有相关数据可以导出");
+        return view;
     }
 
     @ResponseBody
